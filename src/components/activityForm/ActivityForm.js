f72758922f8f0ca63bfe079d41d4b27b2b503b2e
Nils Fischer <nils@designatic.de>
2023-06-02 10:28:44 +0200

    add basic form + basic style

---
+++ src/components/activityForm/ActivityForm.js
@@ -0,0 +1,23 @@
+import "./activityForm.css";
+
+export default function ActivityForm() {
+  return (
+    <>
+      <form>
+        <fieldset className="activityForm-fieldset">
+          <p>Add new Activity:</p>
+          <label htmlFor="name">
+            Name: <input type="text" name="name" id="name" />
+          </label>
+
+          <label htmlFor="goodWeather">
+            Good-weather activity:{" "}
+            <input type="checkbox" name="goodWeather" id="goodWeather" />
+          </label>
+
+          <button type="submit">Submit</button>
+        </fieldset>
+      </form>
+    </>
+  );
+}
